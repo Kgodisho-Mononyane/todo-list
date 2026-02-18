9f3d79f263e7ac6d399366ae7726095d74dc99d2
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-18 12:16:55 +0200

    add event listeners for project button and add button

---
+++ src/DOMController.js
@@ -0,0 +1,15 @@
+export const addTask = (function() {
+    const addBtn = document.querySelector("#add-btn");
+
+    addBtn.addEventListener("click", () => {
+        console.log("Add button clicked");
+    })
+})();
+
+export const projectBtn = (function() {
+    const projectBtn = document.querySelector("#project-btn");
+
+    projectBtn.addEventListener("click", () => {
+        console.log("Project button clicked");
+    })
+})();

5000466c9b40ecc63df3df3b1e9d6983d4422d0c
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-18 13:38:20 +0200

    create functions for dialog boxes and link them to respective event listeners

---
+++ src/toDo.js
@@ -0,0 +1,14 @@
+class ToDo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    //create new class
+    //read class
+    //update class
+    //delete class
+
+}

ed51e5620f97205e44d1217d9992ac7dbeeb281c
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-20 10:23:52 +0200

    add ToDo class and addToDo function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import "./style.css";
+import "./toDo.js"
 import { addTask } from "./DOMController.js";
--- src/toDo.js
+++ src/toDo.js
@@ -4,10 +4,39 @@ class ToDo {
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.completed = false;
   }
 
   //create new class
   //read class
   //update class
+  updateTitle(newTitle) {
+    this.title = newTitle;
+  }
+  
+  updateDescription(newDescription) {
+    this.description = newDescription;
+  }
+  
+  updateDueDate(newDueDate) {
+    this.dueDate = newDueDate;
+  }
+  
+  updatePriority(newPriority) {
+    this.priority = newPriority;
+  };
+
+  completeToDo() {
+    this.completed = true;
+  }
   //delete class
 }
+
+function addToDo(title, description, dueDate, priority) {
+  let toDo = new ToDo(title, description, dueDate, priority);
+  //push to project
+}
+
+// const wakeUp = new ToDo("Wake up", "Wake up at 5AM", "Due Date", "low");
+// wakeUp.updateTitle("Wake Up at 5AM")
+// console.log(wakeUp)

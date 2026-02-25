2a41419805f36ecf930dff24264fa9425b58fad7
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-02-25 11:15:34 +0200

    add project dialog box to DOMController

--- src/DOMController.js
+++ src/DOMController.js
@@ -17,11 +17,53 @@ export const projectBtn = (function () {
 
   projectBtn.addEventListener("click", () => {
     console.log("Project button clicked");
-    addProjectBox();
+    projectDialogBox();
   });
 })();
 
-function addProjectBox() {
-  const projectBox = document.querySelector("#project-dialog");
+function projectDialogBox() {
+  const leftContainer = document.querySelector("#left-container");
+
+  
+  const projectBox = document.createElement("dialog");
+  projectBox.setAttribute("id", "project-dialog");
+  projectBox.setAttribute("closedby", "any") //check if this works
+  leftContainer.appendChild(projectBox);
   projectBox.showModal();
+
+  const projectForm = document.createElement("form");
+  projectForm.setAttribute("action", "");
+  projectForm.setAttribute("method", "post");
+  projectForm.setAttribute("id", "project-form");
+  projectBox.appendChild(projectForm);
+
+  const projectHeader = document.createElement("h2");
+  projectHeader.setAttribute("id", "project-header");
+  projectHeader.textContent = "CREATE PROJECT";
+  projectForm.appendChild(projectHeader);
+
+  const projectNameFormElement = document.createElement("div");
+  projectNameFormElement.setAttribute("class", "form-element");
+  projectNameFormElement.setAttribute("id", "project-name-form-element");
+  projectForm.appendChild(projectNameFormElement);
+
+  const projectNameLabel = document.createElement("label");
+  projectNameLabel.setAttribute("for", "project-name-input");
+  projectNameLabel.setAttribute("class", "form-label");
+  projectNameLabel.setAttribute("id", "project-name-label");
+  projectNameLabel.textContent = "Project Name: "
+  projectNameFormElement.appendChild(projectNameLabel);
+
+  const projectNameInput = document.createElement("input");
+  projectNameInput.setAttribute("type", "text");
+  projectNameInput.setAttribute("class", "form-input");
+  projectNameInput.setAttribute("id", "project-name-input");
+  projectNameFormElement.appendChild(projectNameInput);
+
+  const projectSubmitBtn = document.createElement("button")
+  projectSubmitBtn.setAttribute("id", "project-submit-btn");
+  projectSubmitBtn.setAttribute("type", "submit");
+  projectSubmitBtn.textContent = "SUBMIT";
+  projectForm.appendChild(projectSubmitBtn)
+
 }
